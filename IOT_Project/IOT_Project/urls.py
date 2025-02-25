035f6cde7f9a43c2ad16eed398a4222b89a3ddfc
giripranay <30807375+giripranay@users.noreply.github.com>
2025-02-25 08:10:44 +0000

    Update Home page endpoint

--- IOT_Project/IOT_Project/urls.py
+++ IOT_Project/IOT_Project/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('IOT_app/', include('IOT_app.urls')),
+    path('', include('IOT_app.urls')),
     path('admin/', admin.site.urls),
 ]
 
